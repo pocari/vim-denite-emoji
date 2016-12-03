ef8079e5383f0e34f7add0450488af06baf538e6
pocari <caffelattenonsugar@gmail.com>
2016-12-03 18:32:42 +0900

    initial commit

---
+++ rplugin/python3/denite/source/emoji.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# FILE: emoji.py
+# AUTHOR: pocari <caffelattenonsugar at gmail.com>
+# License: MIT license
+
+from .base import Base
+
+class Base(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+class Source(Base):
+    EMOJI_LIST = {
+        0x1f44b: "WAVING HAND SIGN",
+        0x1f44c: "OK HAND SIGN"
+    }
+
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'emoji'
+        self.kind = 'word'
+
+    def gather_candidates(self, context):
+        candidates = []
+        for code, emoji_name in Source.EMOJI_LIST.items():
+            emoji = chr(code)
+            candidates += [{
+                'word': emoji,
+                'action__text': "{0} ({1})".format(emoji, emoji_name)
+            }]
+        return candidates
